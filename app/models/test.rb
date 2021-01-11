ff02842ccfbf47d05a445255d2216b510ad81669
Stepan <tugunrus@icloud.com>
2021-01-11 16:45:13 +0300

    Completing task
    Adding required methods and user_passed_test.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
+  def self.sortedTestsNamesOfCategory(category_title)
+    joinedTable = Test.joins('JOIN categories ON tests.category_id = categories.id')
+
+    oneCategoryTable = joinedTable.all.where('categories.title == :category_title', category_title: category_title)
+
+    orderedTable = oneCategoryTable.order('tests.title DESC')
+
+    titlesArray = orderedTable.pluck(:title)
+
+    # Или можно в одну строку:
+    # Test.joins('JOIN categories ON tests.category_id = categories.id').where
+    # ("categories.title == :category_title", category_title: category_title).
+    # order('tests.title DESC').pluck(:title)
+  end
 end
