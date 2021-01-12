4d528dbcfb23b2fbf217b751fd3081a4fd6fd43d
Stepan <tugunrus@icloud.com>
2021-01-12 20:24:59 +0300

    user.rb - Использую joins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@
 
 class User < ApplicationRecord
   def completed_tests_of_level(level)
-    passed_tests_ids = UserPassedTest.where(user_id: id).pluck(:test_id)
-
+    joinedTable = Test.joins('JOIN user_passed_tests ON tests.id = user_passed_tests.test_id')
     # Возвращаем тесты конкретного уровня, пройденные пользователем
-    Test.where(id: passed_tests_ids, level: level)
+    joinedTable.where(level: level)
   end
 end
