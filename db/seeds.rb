f3d806cc95f8a44357a1f51c7382c5a23b90b0f7
Stepan <tugunrus@icloud.com>
2021-01-12 11:23:00 +0300

    Исправляю seeds.rb
    Теперь вместо трёх строчек create подряд имеем одну строчку, принимающую массив из 3 хешей.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,28 @@ general_category = Category.create!({ title: 'General' })
 first_test = Test.create!({ title: 'The very first test', level: 1, category_id: general_category.id, user_id: first_user.id })
 
 first_question = Question.create!({ title: 'Which do you choose?', test_id: first_test.id })
-Answer.create!({ title: 'Lesser', correct: false, question_id: first_question.id })
-Answer.create!({ title: 'Bigger', correct: false, question_id: first_question.id })
-Answer.create!({ title: 'Prefer not to choose at all', correct: true, question_id: first_question.id })
+Answer.create!([
+  { title: 'Lesser', correct: false, question_id: first_question.id },
+  { title: 'Bigger', correct: false, question_id: first_question.id },
+  { title: 'Prefer not to choose at all', correct: true, question_id: first_question.id }
+])
 
 second_question = Question.create!({ title: 'Is cat alive?', test_id: first_test.id })
-Answer.create!({ title: 'Yes', correct: false, question_id: second_question.id })
-Answer.create!({ title: 'No', correct: false, question_id: second_question.id })
-Answer.create!({ title: 'I\'m not sure', correct: true, question_id: second_question.id })
+Answer.create!([
+  { title: 'Yes', correct: false, question_id: second_question.id },
+  { title: 'No', correct: false, question_id: second_question.id },
+  { title: 'I\'m not sure', correct: true, question_id: second_question.id }
+  ])
 
 music_category = Category.create!({ title: 'Music' })
 second_test = Test.create!({ title: 'Test about Led Zeppelin!', level: 2, category_id: music_category.id, user_id: first_user.id })
 
 third_question = Question.create!({ title: 'What is pictured on the cover of the "Led Zeppelin IV" album?', test_id: second_test.id })
-Answer.create!({ title: 'A young hunter', correct: false, question_id: third_question.id })
-Answer.create!({ title: 'A purple lion', correct: false, question_id: third_question.id })
-Answer.create!({ title: 'An old countryman', correct: true, question_id: third_question.id })
+Answer.create!([
+  { title: 'A young hunter', correct: false, question_id: third_question.id },
+  { title: 'A purple lion', correct: false, question_id: third_question.id },
+  { title: 'An old countryman', correct: true, question_id: third_question.id }
+  ])
 
 puts "Titles of tests of General category: #{Test.sorted_tests_names_of_category('General')}"
 puts "Titles of tests of Music category: #{Test.sorted_tests_names_of_category('Music')}"
