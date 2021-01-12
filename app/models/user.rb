8efd67a3280466ebedc0934ba9556ee7734dbc17
Stepan <tugunrus@icloud.com>
2021-01-12 11:21:06 +0300

    Исправляю user.rb
    Программа работала неправильно - возвращала тесты, автором которых является пользователь.
    Теперь правильно - теперь в отдельной таблице смотрит, какие тесты прошёл пользователь, сохраняет id, и по ним находит искомые тесты требуемого уровня сложности.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   def completed_tests_of_level(level)
-    Test.where(user_id: id, level: level)
+    passed_tests_ids = UserPassedTest.where(user_id: id).pluck(:test_id)
+
+    # Возвращаем тесты конкретного уровня, пройденные пользователем
+    Test.where(id: passed_tests_ids, level: level)
   end
 end
