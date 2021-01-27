073676145f9899f4a75ffd4dbdaec522f97a5478
Stepan <tugunrus@icloud.com>
2021-01-27 15:38:15 +0300

    Problem with dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@
 
 class User < ApplicationRecord
 
-  # has_and_belongs_to_many :tests
-  has_many :tests_users
+  has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: "Test"
 
+  # Возвращаем тесты конкретного уровня, пройденные пользователем
   def completed_tests_of_level(level)
-    # Возвращаем тесты конкретного уровня, пройденные пользователем
     Test
       .joins('JOIN tests_users ON tests.id = tests_users.test_id')
       .where(level: level)
