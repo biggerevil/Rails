ec339b9169bd27053db0c98fcf6d9b7cf90a1ba8
biggerevil <tugunrus@icloud.com>
2021-02-24 17:56:36 +0300

    Исправляю ошибки

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,6 @@ module ApplicationHelper
 
   def github_url(author, repo)
     link = "https://github.com/#{author}/#{repo}"
-    puts "link = #{link}"
-    link_to 'TestGuru', link
+    link_to 'TestGuru', link, target: :_blank
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,6 +2,10 @@
 
 module QuestionsHelper
   def question_header(test)
-    test.title
+    if @question.new_record?
+      "Create New #{test.title} Question"
+    else
+      "Edit #{test.title} Question"
+    end
   end
 end
