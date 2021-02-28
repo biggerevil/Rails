2db5c8239fe7b23bc01bc84d0d55a4b3eb67f7f1
biggerevil <tugunrus@icloud.com>
2021-02-28 17:54:42 +0300

    [Само задание] Решаю задание

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module TestPassagesHelper
+  def all_questions_helper(test_passage)
+    pluralize(test_passage.test.questions.count, 'question', plural: 'questions')
+  end
 end
