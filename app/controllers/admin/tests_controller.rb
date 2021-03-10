837727371ce9b6cdf42ac6ab62e374965292364b
biggerevil <tugunrus@icloud.com>
2021-03-10 22:02:09 +0300

    Назначение администратора автором теста

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,6 +16,7 @@ class Admin::TestsController < Admin::BaseController
 
   def create
     @test = Test.new(test_params)
+    @test.author = current_user
 
     if @test.save
       redirect_to [:admin, @test]
