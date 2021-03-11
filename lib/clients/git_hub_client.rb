3b7f34069bea1a96170eb26af66db529a784eb68
biggerevil <tugunrus@icloud.com>
2021-03-11 14:06:17 +0300

    Код из скринкаста 12 части (Работа с внешними API)

---
+++ lib/clients/git_hub_client.rb
@@ -0,0 +1,24 @@
+class GitHubClient
+
+  ROOT_ENDPOINT = 'https://api.github.com'
+  ACCESS_TOKEN = '4dd6114392aa89079fb8e789723d4a8f67fde438'
+
+  def initialize
+    @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post('gists') do |request|
+      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Content-Type'] = 'application/json'
+      request.body = params.to_json
+    end
+  end
+
+  private
+
+  def setup_http_client
+    Faraday.new(url: ROOT_ENDPOINT)
+  end
+
+end
