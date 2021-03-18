32355b13080cdb158da4c58c855741b8508cbf9e
biggerevil <tugunrus@icloud.com>
2021-03-19 01:51:43 +0300

    Badge model update

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,10 @@
 class Badge < ApplicationRecord
+
+  has_many :user_badges, dependent: :destroy
+  has_many :users, through: :user_badges
+
+  validates :title, presence: true
+  validates :image_linl, presence: true
+  validates :rule_type, presence: true
+
 end
