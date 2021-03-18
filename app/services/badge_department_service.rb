ec8b8283f8e5a3495af8b93e5927bed7a87a5893
biggerevil <tugunrus@icloud.com>
2021-03-19 01:47:37 +0300

    Start programming specifications

---
+++ app/services/badge_department_service.rb
@@ -0,0 +1,2 @@
+class BadgeDepartmentService
+end
---
+++ app/specifications/badges/first_try_pass_rule_specification.rb
@@ -0,0 +1,7 @@
+module Badges
+  class FirstTryPassRuleSpecification < AbstractRuleSpecification
+    def satisfies?
+      true || false
+    end
+  end
+end
